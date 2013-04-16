c05ba3a0795cd18b23b7606e2b98c338efe02a99
Adrian Schneider <siradrian@gmail.com>
2013-04-15 20:32:04 -0700

    allow swapping of issue/command for easier editing

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,5 +4,11 @@ if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
     die('You must first run composer.');
 }
 
+// XXX
+if (!empty($argv[2]) and preg_match('/^([0-9]+)$/', $argv[1]) and preg_match('/^([a-z]+)$/', $argv[2])) {
+    list($argv[1], $argv[2]) = array($argv[2], $argv[1]);
+    $_SERVER['argv'] = $argv;
+}
+
 $loader = include $file;
 $loader->add('Qissues', __DIR__);

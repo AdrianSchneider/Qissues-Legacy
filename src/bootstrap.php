9ad38ca337b122a49f2c5d2edd2fea0d86e5f051
Adrian Schneider <siradrian@gmail.com>
2013-10-28 21:47:56 -0700

    ignore argv stuff for tests

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,17 +6,19 @@ if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
 
 error_reporting(E_ALL);
 
-// XXX allow swapping of ID and command
-if (!empty($argv[2]) and preg_match('/^([0-9]+)$/', $argv[1]) and preg_match('/^([a-z]+)$/', $argv[2])) {
-    list($argv[1], $argv[2]) = array($argv[2], $argv[1]);
-}
+if (isset($argv)) {
+    // XXX allow swapping of ID and command
+    if (!empty($argv[2]) and preg_match('/^([0-9]+)$/', $argv[1]) and preg_match('/^([a-z]+)$/', $argv[2])) {
+        list($argv[1], $argv[2]) = array($argv[2], $argv[1]);
+    }
 
-// XXX don't require "query" command
-if (!empty($argv[1]) and strpos($argv[1], '-') !== false and strpos($argv[1], 'help') === false) {
-    array_splice($argv, 1, 0, array('query'));
-}
+    // XXX don't require "query" command
+    if (!empty($argv[1]) and strpos($argv[1], '-') !== false and strpos($argv[1], 'help') === false) {
+        array_splice($argv, 1, 0, array('query'));
+    }
 
-$_SERVER['argv'] = $argv;
+    $_SERVER['argv'] = $argv;
+}
 
 $loader = include $file;
 $loader->add('Qissues', __DIR__);

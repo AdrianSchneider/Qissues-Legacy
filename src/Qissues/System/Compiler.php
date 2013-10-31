a232dd5b498792a5f4e5858ec1021d9117a56692
Adrian Schneider <siradrian@gmail.com>
2013-10-31 10:01:52 -0700

    fix compiler

--- src/Qissues/System/Compiler.php
+++ src/Qissues/System/Compiler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Qissues;
+namespace Qissues\System;
 
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Process\Process;
@@ -29,27 +29,30 @@ class Compiler
             ->ignoreVCS(true)
             ->name('*.php')
             ->notName('Compiler.php')
-            ->in(__DIR__.'/..');
+            ->in(__DIR__ . '/../../');
         
         foreach ($finder as $file) {
             $this->addFile($phar, $file);
         }
-        
+
         $finder = new Finder();
         $finder
             ->files()
             ->ignoreVCS(true)
             ->name('*.php')
-            ->in(__DIR__ . '/../../vendor');
+            ->in(__DIR__ . '/../../../vendor');
         
         foreach ($finder as $file) {
             $this->addFile($phar, $file);
         }
+
+        $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../../config/services.yml'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../../config/trackers.yml'));
         
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/autoload.php'));
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_namespaces.php'));
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_classmap.php'));
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/ClassLoader.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../../vendor/autoload.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../../vendor/composer/autoload_namespaces.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../../vendor/composer/autoload_classmap.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../../vendor/composer/ClassLoader.php'));
         $this->addBin($phar);
         
         $phar->setStub($this->getStub());
@@ -65,7 +68,7 @@ class Compiler
      */
     protected function addFile($phar, $file, $strip = true)
     {
-        $path = str_replace(dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR, '', $file->getRealPath());
+        $path = str_replace(dirname(dirname(dirname(__DIR__))).DIRECTORY_SEPARATOR, '', $file->getRealPath());
         
         $content = file_get_contents($file);
         if ($strip) {
@@ -85,7 +88,7 @@ class Compiler
      */
     protected function addBin($phar)
     {
-        $content = file_get_contents(__DIR__.'/../../bin/qissues');
+        $content = file_get_contents(__DIR__.'/../../../bin/qissues');
         $content = preg_replace('{^#!/usr/bin/env php\s*}', '', $content);
         $phar->addFromString('bin/qissues', $content);
     }

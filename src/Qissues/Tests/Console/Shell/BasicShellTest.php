8060145bd1af5a8ca6841c2985f87a49619def21
Adrian Schneider <siradrian@gmail.com>
2013-11-10 06:05:13 -0500

    +test

---
+++ src/Qissues/Tests/Console/Shell/BasicShellTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Qissues\Tests\Console\Shell;
+
+use Qissues\Console\Shell\BasicShell;
+
+class BasicShellTest extends \PHPUnit_Framework_TestCase
+{
+    public function testReturnsOutput()
+    {
+        $shell = new BasicShell();
+        $this->assertEquals('hello world', $shell->run("echo 'hello world'"));
+    }
+
+    public function testEscapesOutput()
+    {
+        $shell = new BasicShell();
+        $this->assertEquals("'hello world'", $shell->escape('hello world'));
+    }
+}

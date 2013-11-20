1e933dd6fb9828ac01a3f59e0c4848ca74cdfedb
Adrian Schneider <siradrian@gmail.com>
2013-11-19 23:59:40 -0800

    delete command fixed

--- src/Qissues/Console/Command/DeleteCommand.php
+++ src/Qissues/Console/Command/DeleteCommand.php
@@ -2,6 +2,7 @@
 
 namespace Qissues\Console\Command;
 
+use Qissues\Model\Number;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
@@ -14,7 +15,7 @@ class DeleteCommand extends Command
     {
         $this
             ->setName('delete')
-            ->setDescription('Open or re-open an issue')
+            ->setDescription('Delete an issue from the tracker')
             ->addArgument('issue', InputArgument::OPTIONAL, 'The issue ID')
             ->addOption('force', null, InputOption::VALUE_NONE, 'Force deletion.', null)
         ;
@@ -22,18 +23,21 @@ class DeleteCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        throw new \Exception('work in progress');
-        $connector = $this->getApplication()->getConnector('BitBucket');
-        if (!$issue = $connector->find($this->getIssueId($input))) {
-            return $output->writeln('<error>Issue not found.</error>');
-        }
-
         if (!$input->getOption('force')) {
             $output->writeln("<error>This operation is destructive.</error> Use the --force option to confirm.");
             return;
         }
 
-        $connector->delete($issue, 'open');
-        $output->writeln("Issue <info>#$issue[id]</info> has been deleted.");
+        $tracker = $this->getApplication()->getTracker();
+        $repository = $tracker->getRepository();
+
+        $number = new Number($this->get('console.input.git_id')->getId($input));
+        if (!$issue = $repository->lookup($number)) {
+            $output->writeln('<error>Issue not found.</error>');
+            return 1;
+        }
+
+        $repository->delete($number);
+        $output->writeln("Issue <info>#$number</info> has been deleted.");
     }
 }

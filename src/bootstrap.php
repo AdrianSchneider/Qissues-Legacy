2ac5e6c305cb2ad48ee5c115daf4a82046f5e108
Adrian Schneider <siradrian@gmail.com>
2013-11-29 09:34:01 -0800

    moved hacky argv trickery from bootstrap to console app

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,18 +6,4 @@ if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
 
 error_reporting(E_ALL);
 
-if (isset($argv)) {
-    // XXX allow swapping of ID and command
-    if (!empty($argv[2]) and preg_match('/^([0-9]+)$/', $argv[1]) and preg_match('/^([a-z]+)$/', $argv[2])) {
-        list($argv[1], $argv[2]) = array($argv[2], $argv[1]);
-    }
-
-    // XXX don't require "query" command
-    if (!empty($argv[1]) and strpos($argv[1], '-') !== false and strpos($argv[1], 'help') === false) {
-        array_splice($argv, 1, 0, array('query'));
-    }
-
-    $_SERVER['argv'] = $argv;
-}
-
 include $file;
